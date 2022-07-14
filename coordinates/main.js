22991c0d0d67af7158f8c1af45cae177b9e4f6cd
simgyuo <gosim8611@gmail.com>
2022-07-14 16:41:09 +0900

    find coordinate

---
+++ coordinates/main.js
@@ -0,0 +1,26 @@
+/* let screenLog = document.querySelector(".screen-log");
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  screenLog = `${mouseX},${mouseY}`;
+});
+ */
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(`${x},${y}`);
+
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
+  tag.innerHTML = `${x}px,${y}px`;
+});

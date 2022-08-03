13b9a0a24a0358f6263b35489088e611ca1318bd
simgyuo <gosim8611@gmail.com>
2022-08-03 15:18:27 +0900

    JS-shopping-improve

--- shopping-app/main.js
+++ shopping-app/main.js
@@ -19,44 +19,61 @@ function onAdd() {
   // 5. 인풋을 초기화 한다.
   input.value = "";
   input.focus();
+}
+let id = 0; // UUID
+function createItem(text) {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+      <div class="item">
+        <span class="item__name">${text}</span>
+          <button class="item__delete">
+        <i class="fa-solid fa-trash-can" data-id=${id}></i>
+          </button>
+    </div>
+  <div class="item__divider"></div>`;
+  id++;
+  // const item = document.createElement("div");
+  // item.setAttribute("class", "item");
 
-  function createItem(text) {
-    const itemRow = document.createElement("li");
-    itemRow.setAttribute("class", "item__row");
+  // const itemName = document.createElement("span");
+  // itemName.setAttribute("class", "item__name");
+  // itemName.innerHTML = text;
 
-    const item = document.createElement("div");
-    item.setAttribute("class", "item");
+  // const deleteBtn = document.createElement("button");
+  // deleteBtn.setAttribute("class", "item__delete");
+  // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  // deleteBtn.addEventListener("click", () => {
+  //   items.removeChild(itemRow);
+  //   input.focus();
+  // });
 
-    const itemName = document.createElement("span");
-    itemName.setAttribute("class", "item__name");
-    itemName.innerHTML = text;
+  // const itemDivider = document.createElement("div");
+  // itemDivider.setAttribute("class", "item__divider");
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "item__delete");
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    deleteBtn.addEventListener("click", () => {
-      items.removeChild(itemRow);
-      input.focus();
-    });
+  // item.appendChild(itemName);
+  // item.appendChild(deleteBtn);
 
-    const itemDivider = document.createElement("div");
-    itemDivider.setAttribute("class", "item__divider");
-
-    item.appendChild(itemName);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
-    return itemRow;
-  }
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
+  return itemRow;
 }
 
 addBtn.addEventListener("click", () => {
   onAdd();
 });
 
-input.addEventListener("keypress", (event) => {
+input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });

aa3d8a32f86c15170ad2e80f4da0344c9f75aeb0
simgyuo <gosim8611@gmail.com>
2022-08-07 18:38:51 +0900

    JS-clone-carrotgame

--- shopping-app/main.js
+++ shopping-app/main.js
@@ -1,7 +1,12 @@
 const items = document.querySelector(".items");
 const input = document.querySelector(".footer__input");
 const addBtn = document.querySelector(".footer__button");
+const form = document.querySelector(".new__form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  onAdd();
+});
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아옴
   const text = input.value;
@@ -59,16 +64,6 @@ function createItem(text) {
   // itemRow.appendChild(itemDivider);
   return itemRow;
 }
-
-addBtn.addEventListener("click", () => {
-  onAdd();
-});
-
-input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    onAdd();
-  }
-});
 
 items.addEventListener("click", (event) => {
   const id = event.target.dataset.id;
